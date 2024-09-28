46606e6c4cd5d5e46555fb1b1921e11b14d7f943
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-28 12:21:27 +0600

    Downloaded the image icons and inported them to the app file, created Items array to store the information about each item in 'Add items' list including id, name, price and imported image. Created Components directory and added OrderDetails.tsx and Item.tsx components to start off. And added types.d.ts file to store files separately.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css';
+import hamburgerImage from './assets/hamburger.png';
+import coffeeImage from './assets/coffee.png';
+import cheeseburgerImage from './assets/cheeseburger.png';
+import teaImage from './assets/tea.png';
+import friesImage from './assets/fries.png';
+import colaImage from './assets/cola.png';
 
-function App() {
+const items = [
+    { id: 1, name: 'Hamburger', price: 80, image: hamburgerImage },
+    { id: 2, name: 'Coffee', price: 70, image: coffeeImage },
+    { id: 3, name: 'Cheeseburger', price: 90, image: cheeseburgerImage },
+    { id: 4, name: 'Tea', price: 50, image: teaImage },
+    { id: 5, name: 'Fries', price: 45, image: friesImage },
+    { id: 6, name: 'Cola', price: 40, image: colaImage },
+];
 
-  return (
-    <>
-<p>Hello world</p>
-    </>
-  );
-}
+const App = () => {
+    return (
+        <div className="app-container">
+        </div>
+    );
+};
 
 export default App;
